18e09053a697eba53377d7460e0cf41bedb36fd8
Danil Gribanov <gribanovwork@yandex.ru>
2022-10-08 19:26:54 +0500

    Сохранил SQL запросы

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express, { Request, Response } from "express";
+/*import pg;*/
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
-
+/*https://github.com/Konstaphy/Task_manager*/
 app.get('/', (req: Request, res: Response) => {
+    /*запрос с бд*/
     res.send('HUY + huy + ultrahuy sdfsdf')
+    /*send в виде JSON "res.json()"*/
 })
 
 app.listen(PORT, () => console.log(`server started on post ${PORT}`))

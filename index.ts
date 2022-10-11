91c391b405724ac411d6a13a1abc2ac583e6e91e
Konstantine <Konstaphy@gmail.com>
2022-10-11 19:04:40 +0500

    Подключил БД

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from "express";
-import Pool from "./db"
+import { pool } from "./db"
 const PORT = process.env.PORT || 8080
+
+// Настройка .env файла с креденшиалами
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 /*const userRouter = require('./routes/user.routes')*/
 
 const app = express()
 /*https://github.com/Konstaphy/Task_manager*/
 app.get('/', async (req: Request, res: Response) => {
-    const resp = await Pool.query("SELECT * FROM institutes")
+    const resp = await pool.query("SELECT * FROM institutes")
     /*запрос с бд*/
     res.json(resp.rows)
     /*send в виде JSON "res.json()"*/

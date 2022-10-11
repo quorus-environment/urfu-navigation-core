e6be36036a561233a10048514ab24df7438ab966
lenaa1 <85735585+lenaa1@users.noreply.github.com>
2022-10-11 23:47:17 +0500

    feature: добавила корсы

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { pool } from "./db"
+import cors from 'cors'
+
 const PORT = process.env.PORT || 8080
 
 // Настройка .env файла с креденшиалами
@@ -10,6 +12,8 @@ dotenv.config()
 
 const app = express()
 /*https://github.com/Konstaphy/Task_manager*/
+
+app.use(cors())
 app.get('/', async (req: Request, res: Response) => {
     const resp = await pool.query("SELECT * FROM institutes")
     /*запрос с бд*/

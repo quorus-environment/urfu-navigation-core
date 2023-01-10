ac4c5a74b89f7adbf6b5e4bc78324f1d663e4ff1
AricsyGIT <aricsybsn@gmail.com>
2023-01-10 05:18:25 +0500

    фикс yaml-файла

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ app.use(cors())
 app.get('/', async (req: Request, res: Response) => {
     // const resp = await pool.query("SELECT * FROM institutes")
     /*запрос с бд*/
-    res.json("проверка")
+    res.json("проверка #1")
     /*send в виде JSON "res.json()"*/
 })
 /*app.use(express.json)
